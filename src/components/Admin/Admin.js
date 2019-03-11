e30145435a4ec2f455bb98ddc488881b8cb79897
Madison Hershkovitz <mlhersh6@gmail.com>
2019-03-10 23:46:57 -0500

    inputs set up

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -23,14 +23,41 @@ componentDidMount() {
     this.getProjects();
 }//end component did mount
 
+getProjects() {
+  this.props.dispatch({ type: 'GET_PROJECT' })
+}
+
 
   
-    render() {
-      
-      return (
-        <div>
+render() {  
+  return (
+      <div>
+        <input placeholder="name"
+        type="text"
+        value={this.state.newProject.name}
+        onChange={this.handleChange('name')}/>
+        <input placeholder="description"
+        type="text" 
+        value={this.state.newProject.description}
+        onChange={this.handleChange('date')}/>
+        <input placeholder="gitHub link" 
+        type="text" 
+        value={this.state.newProject.gitHub}
+        onChange={this.handleChange('gitHub')}/>
+        <input placeholder="website link"
+        type="text"
+        value={this.state.newProject.website}
+        onChange={this.handleChange('website')}/>
+        <input placeholder="date" type="date"
+        value={this.state.newProject.date}
+        onChange={this.handleChange('date')}/>
+        <input placeholder="thumbnail" type="imgUrl"
+        value={this.state.newProject.thumbnail}
+        onChange={this.handleChange('thumbnail')}/>
+
         
-        </div>
+        
+      </div>
       );
     }
   }
